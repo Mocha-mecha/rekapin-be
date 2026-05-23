2dbfb244a439b7e116c7b480252b0ef3831c21b9
alfian <alfianviskei@gmail.com>
2026-05-23 18:41:23 +0700

    Initial commit

---
+++ src/main/java/com/ut/rekapinbe/RekapinBeApplication.java
@@ -0,0 +1,13 @@
+package com.ut.rekapinbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RekapinBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RekapinBeApplication.class, args);
+    }
+
+}
